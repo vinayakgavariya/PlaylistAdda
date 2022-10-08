4778e34575c295e55b8fef2c83f09579c1a6b259
RAGHAV GARG <raghavgarg.hcst.cs20@sgei.org>
2022-10-08 21:34:16 +0530

    Scroll bottom to top

--- index.js
+++ index.js
@@ -17,3 +17,24 @@ function searchPlaylist() {
         }
     }
 }
+
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 20) {
+        $("#toTopBtn").fadeIn();
+      } else {
+        $("#toTopBtn").fadeOut();
+      }
+    });
+  
+    $("#toTopBtn").click(function () {
+      $("html, body").animate(
+        {
+          scrollTop: 0,
+        },
+        1000
+      );
+      return false;
+    });
+  });
